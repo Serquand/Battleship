0c3ee08a95e0ff82193f3cb26acf9f521831362e
Serquand <esteban.vincent@efrei.net>
2022-10-04 13:12:04 +0200

    getegter

--- site/src/router/index.js
+++ site/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import NotFound from '../views/NotFound.vue'
+import Login from '../views/Login.vue'
+import Profil from '../views/Profil.vue'
+import Game from '../views/Game.vue'
+import Ranking from '../views/Ranking.vue'
 
 const routes = [
   {
@@ -8,7 +12,31 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  
+  {
+    path: '/game', 
+    name: "Game", 
+    component: Game
+  },
+  {
+    path: '/ranking', 
+    name: "Ranking", 
+    component: Ranking
+  },
+  {
+    path: '/login', 
+    name: "Login", 
+    component: Login
+  },
+  {
+    path: '/profil', 
+    name: 'Profil', 
+    component: Profil
+  },
+  {
+    path: '/:pathMatching(.*)*', 
+    name: 'NotFound',
+    component: NotFound, 
+  }
 ]
 
 const router = createRouter({
