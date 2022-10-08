a8ffe97ab3ccdb72eaccaaa6ccfbe776f399d4d0
Serquand <esteban.vincent@efrei.net>
2022-10-08 03:39:41 +0200

    Games Models added

---
+++ api/Models/Game.js
@@ -0,0 +1,49 @@
+import sequelize from "./Connection.js"
+import { DataTypes } from "sequelize"
+import Player from './Players.js'
+
+export default sequelize.define("Game", {
+    id: {
+        type: DataTypes.UUID, 
+        defaultValue: DataTypes.UUIDV4, 
+        unique: true, 
+        primaryKey: true,
+        allowNull: false
+    }, 
+
+    result: {
+        type: DataTypes.UUID, 
+        allowNull: false
+    },
+    
+    eloPlayer1: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    }, 
+
+    eloPlayer2: {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+    },
+
+    player1: {
+        type: DataTypes.UUID, 
+        references: {
+            model: Player, 
+            key: 'id'
+        }
+    }, 
+
+    player2: {
+        type: DataTypes.UUID, 
+        references: {
+            model: Player, 
+            key: 'id'
+        }
+    }, 
+}, {
+    timestamps: true,
+    sequelize, 
+    modelName: "Game",
+    tableName: 'Games',
+});
--- api/Models/Setup.js
+++ api/Models/Setup.js
@@ -1,6 +1,8 @@
 import Players from './Players.js'
+import Game from './Game.js'
 
 export default async () => {
     await Players.sync()
+    await Game.sync()
 }
 
