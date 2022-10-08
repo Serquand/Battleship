9fb6ff523a6962dff18b4696945f35ce07b9da46
Serquand <esteban.vincent@efrei.net>
2022-10-09 01:40:03 +0200

    GridShotModal

---
+++ site/src/components/GridShotModal.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="modal grid-shot-modal">
+        <div class="modal-content">
+            <div
+                class="grid-shot"
+                v-for="(n, index) in 100"
+                :key="n"
+            >
+                <div 
+                    :class="['grid-shot-case', arrayTried[index]]"
+                >
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    setup(props) {
+        const arrayDisplay = JSON.parse(JSON.stringify(props.arrayTried)); 
+        return { arrayDisplay }
+    }, 
+    props: {
+        arrayTried: {
+            type: Array, 
+            required: true
+        }
+    }, 
+
+}
+</script>
+
+<style scoped>
+    .modal-content {
+        padding: 0 !important;
+        background: rgb(4, 4, 28);
+        display: grid;
+        grid-template: repeat(10, 32px) / repeat(10, 32px);
+    }
+
+    .modal-content > div {
+        width: 30px;
+        height: 30px;
+        border: .5px solid white;
+        cursor: pointer;
+    }
+
+</style>
