9b70cbab69ca7d4216601896b1ca4ec4556dd919
Serquand <esteban.vincent@efrei.net>
2022-10-11 13:45:26 +0200

    InfoProfil

---
+++ site/src/components/InfoProfil.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="info-profil-container">
+        <div>
+            <img src="/assets/Profil.png" alt="Profil" />
+        </div>
+        <div>
+            <h4 class="username-profil">{{ username }}</h4>
+            <p class="email-profil">{{ email }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        username: {
+            type: String, 
+            required: true
+        }, 
+        email: {
+            type: String, 
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+.info-profil-container {
+    display: flex;
+    gap: 10px;
+}
+
+.info-profil-container div:last-child {
+    justify-content: center;
+    gap: 5px;
+    padding: 5px 0;
+    display: flex;
+    flex-direction: column;
+}
+
+.info-profil-container img {
+    width: 50px;
+    height: 50px;
+}
+
+.info-profil-container h4 {
+    font-weight: 600;
+    font-family: "Roboto", sans-serif;
+    text-decoration: underline;
+}
+</style>
--- site/src/views/Profil.vue
+++ site/src/views/Profil.vue
@@ -1,27 +1,38 @@
 <template>
-    <h1>Profil</h1>
+    <div class="profil-container">
+        <InfoProfil 
+            :email="informationUser.user.Email"
+            :username="informationUser.user.Pseudo"
+        />
+    </div>
 </template>
 
 <script>
 import { url } from "../../config.json";
 import { useAuthStore } from "../store/Auth.js";
+import InfoProfil from "../components/InfoProfil.vue";
+import { ref } from 'vue'
 
 export default {
     setup() {
-        const auth = useAuthStore()
-        return { auth }
+        const auth = useAuthStore(), informationUser = ref({})
+        return { auth, informationUser };
     },
     async created() {
         const requestOptions = {
-            method: "GET", 
+            method: "GET",
             headers: { "Authorization": "Bearer " + this.auth.token }
-        }
-
-        await fetch(url + "/profil/" + this.auth.username, requestOptions)
-    }
+        };
+        this.informationUser = await (await fetch(url + "/profil/" + this.auth.username, requestOptions)).json()
+        console.log(this.informationUser);
+    },
+    components: { InfoProfil }
 }
 </script>
 
 <style>
-
+.profil-container {
+    width: 80%;
+    margin: auto;
+}
 </style>
