400163c5f6e3b2e922af153c3191c4820b291d9e
Serquand <esteban.vincent@efrei.net>
2022-10-11 02:52:48 +0200

    DeleteUser

--- api/Middlewares/Profil/DeleteUser.js
+++ api/Middlewares/Profil/DeleteUser.js
@@ -1,5 +1,31 @@
-const deleteUser = async () => {
-    
-}
+import Players from "../../../Models/Players.js"
+import Games from "../../../Models/Games.js"
 
-export default deleteUser;
+export default async (req, res) => {
+    try {
+        const userId = (await Players.findOne({
+            attributes: ['id'], 
+            where: {
+                Pseudo: req.params.user
+            }
+        })).dataValues.id
+    
+        await Games.update( 
+            { player1: null }, 
+            { where: { player1: userId } }
+        )
+    
+        await Games.update(
+            { player2: null }, 
+            { where: { player2: userId } } 
+        )
+        
+        await Players.destroy({
+            where: { id: userId }
+        })
+
+        res.status(200).json({ information: 'Profil deleted successfully !' })
+    } catch {
+        res.status(400).json({ error: 'An error occured !' })
+    }
+}   
