8c90c33e707e4f76d4829abbc6c15e2786360437
Serquand <esteban.vincent@efrei.net>
2022-10-12 01:08:20 +0200

    Refactor main.js

--- site/src/main.js
+++ site/src/main.js
@@ -3,5 +3,4 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from "pinia"
 
-const pinia = createPinia()
-createApp(App).use(pinia).use(router).mount('#app')
+createApp(App).use(createPinia()).use(router).mount('#app')
