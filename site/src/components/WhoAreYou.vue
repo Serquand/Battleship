0c4532ad9e2152de1f968a6120de999b4258c52e
Esteban <=>
2022-10-19 01:03:09 +0200

    Beginning WhoAreYou

---
+++ site/src/components/WhoAreYou.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>{{ message }}</p>
+</template>
+
+<script>
+export default {
+    props: {
+        message: {
+            type: String,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style></style>
