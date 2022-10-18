ce1e0e5068c6444d32bceb696e3d3dc5e3b523de
Esteban <=>
2022-10-19 01:02:53 +0200

    Beginning WhoAreYou

--- site/src/views/Home.vue
+++ site/src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <WhoAreYou 
+      message=" Developped by Esteban VINCENT, Marty EVUORT and Matthieu EVUORT, this web site is a Web project on which you can play the Battleship's game, 
+                have a ranking, have an account and different other things. It's just a project for a class and won't never be released." 
+    />
+  </div>
 </template>
 
 <script>
+import WhoAreYou from '@/components/WhoAreYou'
+
 export default {
   name: 'Home',
+  components: {
+    WhoAreYou
+  }
 }
 </script>
