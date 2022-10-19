9dd5e2f47c0e38800ddf6658c6053b42742b2285
Esteban <=>
2022-10-19 02:13:20 +0200

    Who are you

--- site/src/components/WhoAreYou.vue
+++ site/src/components/WhoAreYou.vue
@@ -1,9 +1,26 @@
 <template>
-    <p>{{ message }}</p>
+    <div class="who-are-you">
+        <h2>Who are we ?</h2>
+        <div>
+            <p>{{ displayedMessage }}</p>
+            <img src="/assets/WhoAreYou.png" alt="Who are you ?" />
+        </div>
+    </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
+    setup() {
+        const displayedMessage = ref("")
+        return { displayedMessage }
+    },
+    async mounted() {
+        for(let i = 0; i < this.message.length; i++) {
+            this.displayedMessage = this.displayedMessage + this.message.charAt(i)
+            await new Promise(r => setTimeout(r, 20));
+        }
+    },
     props: {
         message: {
             type: String,
@@ -13,4 +30,31 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.who-are-you {
+    width: 60%;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.who-are-you h2 {
+    margin-top: 20px;
+    text-align: center;
+    font-weight: 900;
+    font-size: 25px;
+    margin-bottom: 10px;
+    font-style: italic;
+    text-decoration: underline;
+}
+
+.who-are-you > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 15px;
+    font-weight: 600;
+    line-height: 25px;
+}
+</style>
